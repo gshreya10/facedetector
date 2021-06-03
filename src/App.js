bd299a517a046df5375a97267a449f9c65fdb1cd
Shreya Gupta <shreya.108.sg@gmail.com>
2021-06-03 21:24:58 +0530

    flex

--- src/App.js
+++ src/App.js
@@ -203,7 +203,7 @@ class App extends Component {
       <div className="App">
         <Particles className='particles' params={ParticleParams}/>
         {
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignitems: 'center' }}>  
+          <div className='nav'>  
             <Logo/>
             <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
           </div>
